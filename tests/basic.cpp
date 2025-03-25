47e6dd374ab9a9e4bee9dd7cf8cb62911d0518ea
César Berriot <berriot.cesar@gmail.com>
2025-03-25 16:54:02 +0100

    fixed a unit test

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -6,14 +6,21 @@ extern "C" {
 
 static int value;
 
+static void sleep(int milliseconds);
+
 TEST(thread, basic)
 {	stlt_thread thread = stlt_run_basic
 	(	[]
-		{	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		{	sleep(200);
 			value = 1;
 		}
 	);
+	sleep(100);
 	ASSERT_EQ(value, 0);
 	stlt_join(thread);
 	ASSERT_EQ(value, 1);
+}
+
+static void sleep(int milliseconds)
+{	std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
